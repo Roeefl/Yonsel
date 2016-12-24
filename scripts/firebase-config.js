2871b1fc9ceeefbe76dfb963402817fdafe83298
Roee FL <roeefl@gmail.com>
2016-12-24 16:53:54 +0200

    Initialize firebase settings and interface

---
+++ scripts/firebase-config.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCqA82Pfk-XTVUGfcb7BlNok6TMyVRAyWk",
+    authDomain: "yonsel-ae20b.firebaseapp.com",
+    databaseURL: "https://yonsel-ae20b.firebaseio.com",
+    storageBucket: "yonsel-ae20b.appspot.com",
+    messagingSenderId: "1098171935126"
+  };
+  
+  firebase.initializeApp(config);
